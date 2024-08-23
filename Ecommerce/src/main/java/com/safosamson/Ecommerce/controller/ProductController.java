32fa161cb200af3a8dcb5c99c70862da96882fc5
safo samson <safosamson18@gmail.com>
2024-08-23 13:12:30 +0100

    CRUD done with JPARepository

--- Ecommerce/src/main/java/com/safosamson/Ecommerce/controller/ProductController.java
+++ Ecommerce/src/main/java/com/safosamson/Ecommerce/controller/ProductController.java
@@ -1,4 +1,41 @@
 package com.safosamson.Ecommerce.controller;
 
+import com.safosamson.Ecommerce.model.Product;
+import com.safosamson.Ecommerce.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
 public class ProductController {
+
+    @Autowired
+    ProductService productService;
+    @GetMapping("/products")
+    public List<Product> getProducts() {
+        return productService.getAllProducts();
+    }
+
+    @GetMapping("/product/{prodID}")
+    public Product getProductById(int prodID) {
+        return productService.getProductById(prodID);
+    }
+
+    @PostMapping("/product")
+    public Product addProduct( @RequestBody Product product) {
+        return productService.addProduct(product);
+    }
+
+    @PutMapping("/product")
+    public Product updateProduct( @RequestBody Product product) {
+        return productService.updateProduct(product);
+    }
+
+    @DeleteMapping("/product/{prodID}")
+    public void deleteProduct(@PathVariable int prodID) {
+        productService.deleteProduct(prodID);
+    }
+
+
 }
--- Ecommerce/src/main/java/com/safosamson/Ecommerce/model/Product.java
+++ Ecommerce/src/main/java/com/safosamson/Ecommerce/model/Product.java
@@ -1,4 +1,51 @@
 package com.safosamson.Ecommerce.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+@Entity // This tells Hibernate to make a table out of this class
 public class Product {
+    @Id
+    @JsonProperty("prodID") // change the name of the field in the JSON response from prodId to prodID
+    private int prodId;
+
+    private String prodName;
+    private int price;
+
+    public Product() {
+    }
+
+    public Product(int prodID, String prodName, int price) {
+        this.prodId = prodID;
+        this.prodName = prodName;
+        this.price = price;
+    }
+
+    public int getProdId() {
+        return prodId;
+    }
+
+    public void setProdId(int prodId) {
+        this.prodId = prodId;
+    }
+
+    public String getProdName() {
+        return prodName;
+    }
+
+    public void setProdName(String prodName) {
+        this.prodName = prodName;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
 }
